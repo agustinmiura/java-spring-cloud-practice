0dd86993fed1db2ecdac143df44f0c2147eefa57
Agustin Miura <agustin.miura@gmail.com>
2020-09-11 17:51:28 -0300

    Limits service

--- src/main/java/ar/com/miura/limitsservice/LimitConfigurationController.java
+++ src/main/java/ar/com/miura/limitsservice/LimitConfigurationController.java
@@ -1,6 +1,7 @@
 package ar.com.miura.limitsservice;
 
 import ar.com.miura.limitsservice.bean.LimitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LimitConfigurationController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     @ResponseBody
     public LimitConfiguration getLimitFromConfig() {
-        return new LimitConfiguration(1,100);
+        return new LimitConfiguration(configuration.getMin(), configuration.getMax());
     }
 
 }
