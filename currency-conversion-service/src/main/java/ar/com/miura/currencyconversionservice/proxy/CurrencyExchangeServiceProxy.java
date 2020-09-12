dce9f144631143fcaf6283f2685dd09c65a3745e
Agustin Miura <agustin.miura@gmail.com>
2020-09-12 19:22:40 -0300

    Make Feing calls using Zuul api gateway

--- currency-conversion-service/src/main/java/ar/com/miura/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/ar/com/miura/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -1,14 +1,19 @@
 package ar.com.miura.currencyconversionservice.proxy;
 
 import ar.com.miura.currencyconversionservice.domain.CurrencyConversionBean;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-service")
-@RibbonClient(name="currency-exchange-service")
+/**
+ * @FeignClient(name="currency-exchange-service") : Call Feing client using Eureka service discovery.
+ *                                       Requires : @GetMapping("/currency-exchange/from/{from}/to/{to}")
+ * @RibbonClient(name="currency-exchange-service") : Call Using Ribbon client that needs the addresses of the services.
+ *                                       Requires : @GetMapping("/currency-exchange/from/{from}/to/{to}")
+ *
+ */
+@FeignClient(name="netflix-zuul-api-gateway-server")
 public interface CurrencyExchangeServiceProxy {
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
