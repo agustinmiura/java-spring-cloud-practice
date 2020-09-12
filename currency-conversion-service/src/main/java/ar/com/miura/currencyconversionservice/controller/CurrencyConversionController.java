cb8f758163ea1b17d2c9c4d8a49bfcd93c1d8ca5
Agustin Miura <agustin.miura@gmail.com>
2020-09-12 00:32:05 -0300

    Added Feign client and Ribbon client to make requests

--- currency-conversion-service/src/main/java/ar/com/miura/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/ar/com/miura/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,6 +1,8 @@
 package ar.com.miura.currencyconversionservice.controller;
 
 import ar.com.miura.currencyconversionservice.domain.CurrencyConversionBean;
+import ar.com.miura.currencyconversionservice.proxy.CurrencyExchangeServiceProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,13 +15,20 @@ import java.util.HashMap;
 @RestController
 public class CurrencyConversionController {
 
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
+
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        return proxy.retrieveExchangeValue(from, to);
+        /*
         var uriVariables = new HashMap<String, String>();
         uriVariables.put("from", from);
         uriVariables.put("to", to);
         ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
         var response = responseEntity.getBody();
         return new CurrencyConversionBean(response.getId(), from,to,response.getConversionMultiple(), quantity,quantity.multiply(response.getConversionMultiple()),response.getPort());
+        */
     }
+
 }
