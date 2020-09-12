5f09366c3f75623d5a8d0558d83d0c8e4568bb94
Agustin Miura <agustin.miura@gmail.com>
2020-09-12 01:17:37 -0300

    Fixed client discovery

--- currency-conversion-service/src/main/java/ar/com/miura/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/ar/com/miura/currencyconversionservice/controller/CurrencyConversionController.java
@@ -11,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
@@ -20,15 +21,17 @@ public class CurrencyConversionController {
 
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
-        return proxy.retrieveExchangeValue(from, to);
-        /*
-        var uriVariables = new HashMap<String, String>();
+        Map<String, String> uriVariables = new HashMap<>();
         uriVariables.put("from", from);
         uriVariables.put("to", to);
         ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
-        var response = responseEntity.getBody();
-        return new CurrencyConversionBean(response.getId(), from,to,response.getConversionMultiple(), quantity,quantity.multiply(response.getConversionMultiple()),response.getPort());
-        */
+        CurrencyConversionBean response = responseEntity.getBody();
+        return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
 
+    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+        return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
+    }
 }
--- currency-conversion-service/src/main/java/ar/com/miura/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/ar/com/miura/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name="currency-exchange-service")
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")       //where {from} and {to} are path variable
-    public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to); //from map to USD and to map to INR
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
