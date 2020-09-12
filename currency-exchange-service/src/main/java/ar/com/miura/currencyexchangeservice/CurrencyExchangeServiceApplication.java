6da7f419a840af847129c697ffbafd4333a47015
Agustin Miura <agustin.miura@gmail.com>
2020-09-12 00:56:38 -0300

    Added registry action to microservices

--- currency-exchange-service/src/main/java/ar/com/miura/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/ar/com/miura/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,7 +2,9 @@ package ar.com.miura.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
 
