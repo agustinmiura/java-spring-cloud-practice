9b0835af16a93eb784cb3bcc7740d8fcfbb451d6
Agustin Miura <agustin.miura@gmail.com>
2020-09-13 01:03:32 -0300

    Finished fault tolerant protection

--- limits-service/src/main/java/ar/com/miura/limitsservice/LimitConfigurationController.java
+++ limits-service/src/main/java/ar/com/miura/limitsservice/LimitConfigurationController.java
@@ -1,6 +1,7 @@
 package ar.com.miura.limitsservice;
 
 import ar.com.miura.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +17,16 @@ public class LimitConfigurationController {
     @ResponseBody
     public LimitConfiguration getLimitFromConfig() {
         return new LimitConfiguration(configuration.getMin(), configuration.getMax());
+    }
+
+    @GetMapping("/fault-tolerant-example")
+    @HystrixCommand(fallbackMethod = "fallbackGetConfiguration")
+    public LimitConfiguration getLimitFromConfigFallback() {
+        throw new RuntimeException("Not Available");
+    }
+
+    public LimitConfiguration fallbackGetConfiguration() {
+        return new LimitConfiguration(9,999);
     }
 
 }
